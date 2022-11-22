4f31190e87bf90a9545be9f68b9192a4ce1180b4
william97117 <106554043+william97117@users.noreply.github.com>
2022-11-22 23:37:15 +0100

    Update customer.py

--- Data/services/customer.py
+++ Data/services/customer.py
@@ -2,9 +2,23 @@ import json
 import requests
 import pandas as pd
 import os
-import glob
+import csv
+from fastapi import FastAPI
 
-url= "172.50.0.4:8002"
-response = requests.request("GET", url).json()
-df = pd.DataFrame(response['data.csv'])
-df.to_csv('data.csv')
+# creating a GET request
+f = requests.get("172.50.0.4:8002")
+    customer=json.loads(f.read())
+    headers = customer[0].keys()
+    
+def transform_data_into_csv(n_files=None):
+    parent_folder = '/app/raw_files'
+    files = sorted(os.listdir(parent_folder), reverse=True)
+    if n_files:
+        files = files[:n_files]
+    dfs = []
+    df = pd.DataFrame(dfs)
+    
+    writer =csv.DictWriter(f,fieldnames=headers)
+    writer.writeheader()
+    for custo in customer:
+        writer.writerow(custo)
